2b845fe104331f0b19f5d2560854dd189999a09a
Xiaoqian <shiclaire@163.com>
2018-09-27 14:26:49 +1200

    Update switch_wpNew.sh

--- switch_wpNew.sh
+++ switch_wpNew.sh
@@ -2,7 +2,7 @@
 
 cd ~/
 echo "Starting wordpressNew..."
-docker-compose -f wordpressNew/docker-compose.yml up -d
+docker-compose -f wordpress2/docker-compose.yml up -d
 
 echo "Switch nginx to wordpressNew..."
 rm /etc/nginx/sites-enabled/*
@@ -10,4 +10,4 @@ cp nginx/wp2 /etc/nginx/sites-enabled/
 service nginx reload
 
 echo "Stopping wordpressOld..."
-docker-compose -f wordpressOld/docker-compose.yml down
+docker-compose -f wordpress1/docker-compose.yml down
