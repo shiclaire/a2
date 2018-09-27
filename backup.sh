0ed778a426746cba90b18369dd55df7b7e0beaa4
Xiaoqian <shiclaire@163.com>
2018-09-27 17:53:35 +1200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@
 
 cd ~/
 
-wget https://raw.githubusercontent.com/LiLoveShi/OMGOMGOMG/master/backup_db.sh
+wget https://raw.githubusercontent.com/shiclaire/a2/master/backupdata.sh
 
 if [ "$(crontab -l | grep 'no crontab' | wc -l)" -gt 0 ];then
 
@@ -18,7 +18,7 @@ elif [ "$(crontab -l | grep backup_db.sh | wc -l)" -eq 0 ];then
 
     crontab -l > mycron
 
-    echo "*/5 * * * * /bin/bash /home/ubuntu/backup_db.sh &" >> mycron
+    echo "*/5 * * * * /bin/bash /home/ubuntu/backupdata.sh &" >> mycron
 
     crontab mycron
 
