bd54a7617a838fce906649537680a787745f156c
Xiaoqian <shiclaire@163.com>
2018-10-03 21:45:53 +1300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,37 +1,21 @@
 #!/bin/bash
 
-
-
-today=`date '+%Y%m%d'`
-
+today=`date "+%Y-%m-%d %H:%M:%S %w"`
 bakdir=wordpress-$today
 
-
-
 mkdir /home/$bakdir
-
 mkdir /home/$bakdir/site
-
 mkdir /home/$bakdir/mysql
 
-
-
 bakfile=/home/wordpress-${today}.tar.gz
-
 [ -f $bakfile ] && rm -rf $bakfile
 
 
-
+#Here, the Password is your database password you set. CHANGE it make sure is yours password!!!
 mysqldump -uroot -p"1234" -h127.0.0.1 wordpress --all-databases > /home/$bakdir/wordpress_db.sql
 
-
-
 tar zcvfP ${bakfile} /home/$bakdir >/dev/null
-
 rm -rf /home/$bakdir
 
-
-
-
-
-echo $today $bakfile >> /var/log/wp_bak.log
+#You can find backup time in /var/log/wp_bak.log
+echo $bakfile >> /var/log/wp_bak.log
