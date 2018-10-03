4b80d3e9452acebb30a68d56f94bfa516a1c3fbc
Xiaoqian <shiclaire@163.com>
2018-10-03 20:39:18 +1300

    Update time.sh

--- time.sh
+++ time.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-cd ~/
 
-wget https://raw.githubusercontent.com/Bowbee/2018_Group13/master/backup_wordpress.sh?token=AnnFFxSCIL26DUdU8jrIzn-wgFdE6DJVks5bvaMYwA%3D%3D
+wget https://raw.githubusercontent.com/shiclaire/a2/master/bu.sh
+apt-get install cron
 
 if [ "$(crontab -l | grep 'no crontab' | wc -l)" -gt 0 ];then
-    echo "*/1 * * * * /bin/bash /home/ubuntu/backup_wordpress.sh &" > mycron
+    echo "*/1 * * * * /bin/bash /home/ubuntu/bu.sh &" > mycron
     crontab mycron
     rm mycron
 
 elif [ "$(crontab -l | grep backupdata.sh | wc -l)" -eq 0 ];then
     crontab -l > mycron
-    echo "*/1 * * * * /bin/bash /home/ubuntu/backup_wordpress.sh &" >> mycron
+    echo "*/1 * * * * /bin/bash /home/ubuntu/bu.sh &" >> mycron
     crontab mycron
     rm mycron
 
